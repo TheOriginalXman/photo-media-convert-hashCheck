c620624824e25713b5c0385a92e8a5630a40ff16
TheOriginalXman <prajanch@gmail.com>
2023-02-06 14:06:45 -0800

    Update PhotoConverter.py

--- photoConverter/PhotoConverter.py
+++ photoConverter/PhotoConverter.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+import shutil
 import subprocess
 import platform
 import logging
@@ -46,6 +46,30 @@ class PhotoConverter:
         self.root_path = root_path
         self.root_dir = None
 
+    def convert(self, input_formats = None, output_format = None):
+
+            # if input_formats are not specified, use the input_ext attribute of the class
+            if not input_formats:
+                input_formats = self.input_ext
+            # if output_format is not specified, use the output_ext attribute of the class
+            if not output_format:
+                output_format = self.output_ext
+                
+            # Check if root directories have been specified in the config
+            if self.root_directories:
+                # If root directories have been specified, loop through each root directory
+                if len(self.root_directories) > 0:
+                    for root in self.root_directories:
+                        self.root_dir = root
+                        self._convert(input_formats, output_format)
+                else:
+                    # If no root directories have been specified, log the error
+                    self.logger.error('No root directories specified in config')
+            # If root path has been specified, use that
+            elif self.root_path:
+                self.root_dir = self.root_path
+                self._convert(input_formats, output_format)
+
     def _configure_logger(self):
         # dump all log levels to file
         log_level = self.config.get('logLevel', "INFO")
@@ -63,17 +87,23 @@ class PhotoConverter:
         if self.config.get('singleFileLog', False):
             file_handler = logging.FileHandler(self.log_file, mode='a')
         else:
-            file_handler = logging.FileHandler(self.log_file + currentDateTime(), mode='w')
+            file_handler = logging.FileHandler(currentDateTime() + ' ' + self.log_file , mode='w')
 
         file_handler.setLevel(numeric_log_level)
 
-        # create a formatter for the logs
-        formatter = logging.Formatter('%(asctime)s - %(process)d - %(thread)d - %(name)s - %(funcName)s - %(lineno)d - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
+        # create a console handler to log to the console
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
 
+        # create a formatter for the logs
+        file_formatter = logging.Formatter('%(asctime)s - %(process)d - %(thread)d - %(name)s - %(funcName)s - %(lineno)d - %(levelname)s - %(message)s')
+        console_formatter = logging.Formatter('%(asctime)s - %(funcName)s - %(lineno)d - %(levelname)s - %(message)s')
+        file_handler.setFormatter(file_formatter)
+        console_handler.setFormatter(console_formatter)
         # add the file handler to the logger
         self.logger.addHandler(file_handler)
-        
+        self.logger.addHandler(console_handler)
+
     def install_dependencies(self):
         if platform.system() == "Darwin":
             try:
@@ -111,19 +141,10 @@ class PhotoConverter:
 
         # Start the directory walk
         for dirpath, dirnames, filenames in os.walk(self.root_dir):
-            # Remove all the exclusion folders and files from the directory walk
-            exclusion_folders = self.exclusions.get('folderNames',[])
-            exclusion_folders.append(self.converted_folder_name)
-            for folder in exclusion_folders:
-                if folder in dirnames:
-                    dirnames.remove(folder)
-            self.logger.debug('Removed Excluded Folders')
-            exclusion_files = self.exclusions.get('fileNames',[])
-            for file in exclusion_files:
-                if file in filenames:
-                    filenames.remove(file)
             
-            self.logger.debug('Removed Excluded Files')
+            dirnames = self._remove_excluded_folders_from_traverse_path(dirnames)
+
+            dirnames = self._remove_excluded_files_from_traverse_path(filenames)
 
             self._convert_process_directories(dirpath, filenames, input_formats, output_format)
             
@@ -143,7 +164,7 @@ class PhotoConverter:
     def _convert_process_file(self, dirpath, file, input_formats, output_format, output_folder):
         # Get the full path of the input file
         input_file = os.path.join(dirpath, file)
-        self.logger.info('Input File Path: {0}'.format(input_file))
+        self.logger.debug('Input File Path: {0}'.format(input_file))
 
         # Get file extension
         extension = os.path.splitext(file)[1].lower()
@@ -152,7 +173,7 @@ class PhotoConverter:
         if self._is_file_excluded(extension,input_file, input_formats):
             self.logger.debug('File excluded: {0}'.format(input_file))
             return
-
+        
         # Create the converted photo folder if it doesn't exist
         if not os.path.exists(output_folder):
             os.mkdir(output_folder)
@@ -161,12 +182,14 @@ class PhotoConverter:
         
         # Get the output file name
         output_file = os.path.join(output_folder, file.replace(extension, '.' + output_format).replace(extension.upper(), '.' + output_format))
-        self.logger.info('Output file: {0}'.format(output_file))
+
         # Check if the output file already exists
         if os.path.exists(output_file):
             self.logger.debug('Output file already exists: {0}'.format(output_file))
             return
         self.logger.info('Converting Image')
+        self.logger.info('Input File Path: {0}'.format(input_file))
+        self.logger.info('Output file: {0}'.format(output_file))
         # Convert the file based on the file extension
         if(extension == '.heic'):
             if platform.system() == "Darwin":
@@ -178,6 +201,7 @@ class PhotoConverter:
         
         if(os.path.exists(output_file)):
             self.logger.info('Successfully Converted Photo')
+            self._remove_orientation(output_file, output_format)
         else:
             self.logger.warning('Failed to convert file: {}'.format(input_file))
 
@@ -203,6 +227,36 @@ class PhotoConverter:
             self.logger.debug('Skipping extension is not in input formats list')
             return True
 
+    def _remove_excluded_files_from_traverse_path(self, fileNames):
+        # Remove all the exclusion folders from the directory walk
+        exclusion_files = self.exclusions.get('fileNames',[])
+        for file in exclusion_files:
+            if file in fileNames:
+                fileNames.remove(file)
+        
+        self.logger.debug('Removed Excluded Files')
+
+        return fileNames
+    
+    def _remove_excluded_folders_from_traverse_path(self, folderNames):
+        exclusion_folders = self.exclusions.get('folderNames',[])
+        exclusion_folders.append(self.converted_folder_name)
+        for folder in exclusion_folders:
+            if folder in folderNames:
+                folderNames.remove(folder)
+        
+        self.logger.debug('Removed Excluded Folders')
+
+        return folderNames
+
+    def _remove_orientation(self, file_path, img_format):
+        try:
+            img = Image.open(file_path)
+            img.save(file_path, img_format, exif=b"")
+            img.close()
+        except Exception as e:
+            self.logger.error(f'Error removing orientation information {file_path}')
+  
     def convert_heic_linux(self, input_file, output_file, output_format):
         # Convert the HEIC file to JPG
         self.logger.debug('Converting HEIC on Linux')
@@ -226,33 +280,57 @@ class PhotoConverter:
         # Convert the input file to the output format
         try:
             with Image.open(input_file) as img:
-                img.save(output_file, format=output_format)
+                img.save(output_file, output_format)
         except Exception as e:
             self.logger.error(f'Error converting {input_file} to {output_file}. Error: {e}\n')
-
-    def convert(self, input_formats = None, output_format = None):
-
-        # if input_formats are not specified, use the input_ext attribute of the class
-        if not input_formats:
-            input_formats = self.input_ext
-        # if output_format is not specified, use the output_ext attribute of the class
-        if not output_format:
-            output_format = self.output_ext
-            
-        # Check if root directories have been specified in the config
-        if self.root_directories:
-            # If root directories have been specified, loop through each root directory
-            if len(self.root_directories) > 0:
-                for root in self.root_directories:
-                    self.root_dir = root
-                    self._convert(input_formats, output_format)
-            else:
-                # If no root directories have been specified, log the error
-                self.logger.error('No root directories specified in config')
-        # If root path has been specified, use that
-        elif self.root_path:
-            self.root_dir = self.root_path
-            self._convert(input_formats, output_format)
-
-        
     
+    def remove_all_converted_files(self, traversing_directories=[], removal_folder_name=None):
+        if not traversing_directories and self.root_directories:
+            traversing_directories = self.root_directories
+        
+        elif not traversing_directories and self.root_path:
+                traversing_directories.append(self.root_path)
+        elif not traversing_directories:
+            self.logger.warning(f'No Removal Path Provided, No files were deleted.')
+            return
+        
+        if not removal_folder_name and self.converted_folder_name:
+                removal_folder_name = self.converted_folder_name
+        elif not removal_folder_name:
+                self.logger.warning(f'No Folder Name Provided, No files were deleted.')
+                return 
+
+        for traversing_directory in traversing_directories:
+
+            if not os.path.exists(traversing_directory):
+                self.logger.warning(f'Traversing Directory does not exist: %s' % traversing_directory)
+                return
+            
+            for dirpath, dirnames, filenames in os.walk(traversing_directory):
+                # dirnames = self._remove_excluded_folders_from_traverse_path(dirnames)
+                # filenames = self._remove_excluded_files_from_traverse_path(filenames)
+
+                self.remove_converted_files(dirpath, removal_folder_name)
+
+    def remove_converted_files(self, conversion_parent_folder_path=None, removal_folder_name=None):
+        if not conversion_parent_folder_path:
+            self.logger.warning(f'No Removal Path Provided, No files were deleted.')
+            return
+
+        if not removal_folder_name and self.converted_folder_name:
+            removal_folder_name = self.converted_folder_name
+        elif not removal_folder_name and not self.converted_folder_name:
+            self.logger.warning(f'No Folder Name Provided, No files were deleted.')
+            return
+        
+        removal_path = os.path.join(conversion_parent_folder_path, removal_folder_name)
+        
+        if os.path.exists(removal_path):
+            self.logger.info(f'Removing path and all contents from: {removal_path}')
+
+            shutil.rmtree(removal_path)
+
+            if os.path.exists(removal_path):
+                self.logger.error(f'Path was not removed successfully')
+            else:
+                self.logger.info(f'Path was successfully removed')
